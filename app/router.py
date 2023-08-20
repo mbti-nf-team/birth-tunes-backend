e74b553d26052e34ff74f9236a52bdb6c1324ea9
Eunjeong Park <ejolie.dev@gmail.com>
2023-08-20 21:02:31 +0900

    Add description for request parameter

--- app/router.py
+++ app/router.py
@@ -1,6 +1,6 @@
 import datetime
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Path, Query
 from sqlalchemy.orm import Session
 
 from . import crud, schemas
@@ -14,7 +14,11 @@ router = APIRouter()
     description="날짜로 노래 조회",
     response_model=schemas.FindSongResponse,
 )
-def get_song(music_chart_id: int, date: datetime.date, db: Session = Depends(get_db)):
+def get_song(
+    music_chart_id: int = Path(..., title="뮤직 차트 ID", example=1),
+    date: datetime.date = Query(..., title="조회할 날짜", example="1995-05-03"),
+    db: Session = Depends(get_db),
+):
     db_song = crud.get_song_by_date_and_music_chart(
         db=db,
         date=date,
