9dbd04053a51577edea5d469bed3c05e0dd58298
박은정 <ejolie.dev@gmail.com>
2023-09-03 21:53:07 +0900

    Change API spec for getting song info (#1)

--- app/crud.py
+++ app/crud.py
@@ -6,12 +6,22 @@ from sqlalchemy.orm import Session
 from . import models
 
 
+# deprecated
 def get_song_by_date_and_music_chart(
     db: Session, date: datetime.date, music_chart_id: int
 ):
     return (
         db.query(models.Song)
         .filter(models.Song.music_chart_id == music_chart_id)
+        .filter(models.Song.base_date <= date)
+        .order_by(desc(models.Song.base_date))
+        .first()
+    )
+
+
+def get_song_by_date(db: Session, date: datetime.date):
+    return (
+        db.query(models.Song)
         .filter(models.Song.base_date <= date)
         .order_by(desc(models.Song.base_date))
         .first()
--- app/router.py
+++ app/router.py
@@ -11,10 +11,11 @@ router = APIRouter()
 
 @router.get(
     "/music-charts/{music_chart_id}/song",
-    description="날짜로 노래 조회",
-    response_model=schemas.FindSongResponse,
+    description="뮤직 차트 ID랑 날짜로 노래 조회 (삭제 예정)",
+    response_model=schemas.GetSongResponseDeprecated,
+    deprecated=True,
 )
-def get_song(
+def get_song_deprecated(
     music_chart_id: int = Path(..., title="뮤직 차트 ID", example=1),
     date: datetime.date = Query(..., title="조회할 날짜", example="1995-05-03"),
     db: Session = Depends(get_db),
@@ -23,6 +24,26 @@ def get_song(
         db=db,
         date=date,
         music_chart_id=music_chart_id,
+    )
+    if db_song is None:
+        raise HTTPException(status_code=404, detail="Song not found")
+    return db_song
+
+
+@router.get(
+    "/songs/{year}/{month}/{day}",
+    description="특정 날짜에 1위한 노래 조회",
+    response_model=schemas.GetSongResponse,
+)
+def get_song(
+    year: int = Path(..., ge=1981, le=2023, title="조회할 년도", example=1995),
+    month: int = Path(..., ge=1, le=12, title="조회할 월", example=5),
+    day: int = Path(..., ge=1, le=31, title="조회할 일", example=3),
+    db: Session = Depends(get_db),
+):
+    db_song = crud.get_song_by_date(
+        db=db,
+        date=datetime.date(year, month, day),
     )
     if db_song is None:
         raise HTTPException(status_code=404, detail="Song not found")
