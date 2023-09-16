e3f53f5a829b5e389c1345dbb3220a42df9fa0c6
Eunjeong Park <ejolie.dev@gmail.com>
2023-09-16 15:50:21 +0900

    Set pool_pre_poing option true

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 from .config import get_settings
 
-engine = create_engine(get_settings().DATABASE_URL)
+engine = create_engine(get_settings().DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
